dc62aebe0ea824da1cbbaeb915bdc3e9c5a6e60d
Michaiah Bos <michaiahbos@yahoo.com>
2023-11-28 21:02:10 -0600

    wip

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 // Suggested initial states
-const initialMessage = '';
-const initialEmail = '';
+const initialMessage = "";
+const initialEmail = "";
 const initialSteps = 0;
 const initialIndex = 4; // the index the "B" is at
 
@@ -10,57 +10,49 @@ export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
   const [index, setIndex] = useState(initialIndex); // slice of state
-  // index is a piece of state that represents the current index of the "B" character on a 3 by 3 grid
-  // setIndex is a function provided by useState to update the value of idex
-  // initial index is set to 4, indicationt the intital position of the "B" character
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [steps, setSteps] = useState(initialSteps);
 
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-   let x; //initializing x with no value
-   let y; //initalizing y with no value
-   if(index === 0) {
-     x = 1; 
-     y = 1
-   }
-   else if(index === 1) {
-    x = 2;
-    y = 1;
-   }
-   else if(index === 2) {
-    x = 3;
-    y = 1
-   } 
-   else if(index === 3){
-    x = 1;
-    y = 2
-   }
-   else if(index === 4){
-    x = 2;
-    y = 2
-   } 
-   else if (index === 5){
-    x = 3;
-    y = 2
-   } 
-   else if (index === 6){
-    x = 1;
-    y = 3
-   }
-   else if (index === 7){
-    x = 2;
-    y = 3
-   } 
-   else if (index === 8){
-    x = 3;
-    y = 3
-   }
-   
-   return [x, y];
+    let x; //initializing x with no value
+    let y; //initalizing y with no value
+    if (index === 0) {
+      x = 1;
+      y = 1;
+    } else if (index === 1) {
+      x = 2;
+      y = 1;
+    } else if (index === 2) {
+      x = 3;
+      y = 1;
+    } else if (index === 3) {
+      x = 1;
+      y = 2;
+    } else if (index === 4) {
+      x = 2;
+      y = 2;
+    } else if (index === 5) {
+      x = 3;
+      y = 2;
+    } else if (index === 6) {
+      x = 1;
+      y = 3;
+    } else if (index === 7) {
+      x = 2;
+      y = 3;
+    } else if (index === 8) {
+      x = 3;
+      y = 3;
+    }
+
+    return [x, y];
   }
 
   function getXYMessage() {
-    const [x, y] = getXY()
+    const [x, y] = getXY();
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
@@ -68,7 +60,6 @@ export default function AppFunctional(props) {
     return message;
   }
   // console.log(getXYMessage())
- 
 
   function reset() {
     // Use this helper to reset all states to their initial values.
@@ -80,66 +71,92 @@ export default function AppFunctional(props) {
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
 
-    // in a 3 column grid, indices 0, 3, 6 represent the left edge, and indices 2, 5 and 8 represent the right edge 
-    if (direction === 'left' && index % 3 !== 0) { // check if direction is left, and ensures that moving left is possible 
+    // in a 3 column grid, indices 0, 3, 6 represent the left edge, and indices 2, 5 and 8 represent the right edge
+    if (direction === "left" && index % 3 !== 0) {
+      // check if direction is left, and ensures that moving left is possible
       // because if index is 0, 3 or 6.. % 3 will be 0
-      return index -1;
-    } else if (direction === 'right' && (index + 1) % 3 !== 0) { 
+      return index - 1;
+    } else if (direction === "right" && (index + 1) % 3 !== 0) {
       return index + 1;
-    } else if (direction === 'up' && index >= 3){
-      return index -3;
-    } else if (direction === 'down' && index < 6){
+    } else if (direction === "up" && index >= 3) {
+      return index - 3;
+    } else if (direction === "down" && index < 6) {
       return index + 3;
     } else {
       return index;
     }
-
   }
 
   function move(evt) {
-   
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    const direction = evt.target.id;
+    const nextIndex = getNextIndex(direction);
+    if (nextIndex !== index) {
+      setIndex(nextIndex);
+      setSteps(steps + 1);
+      setMessage(initialMessage);
+    } else {
+      setMessage(`you can't go ${direction}`);
+    }
   }
-
 
   function onChange(evt) {
     // You will need this to update the value of the input.
+    const { id, value } = evt.target;
+    if (id === "email") {
+      setEmail(value);
+    }
   }
 
   function onSubmit(evt) {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault();
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates {getXYMessage()}</h3>
+        <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
+            {idx === index ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={move}>
+          LEFT
+        </button>
+        <button id="up" onClick={move}>
+          UP
+        </button>
+        <button id="right" onClick={move}>
+          RIGHT
+        </button>
+        <button id="down" onClick={move}>
+          DOWN
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="type email"
+          value={email}
+          onChange={onChange}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
